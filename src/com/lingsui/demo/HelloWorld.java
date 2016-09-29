8793b16434501999c5fbfb589d99d81f348f01f3
caoping <1803498631@qq.com>
2016-09-29 17:37:26 +0800

    Initial commit

---
+++ src/com/lingsui/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.lingsui.demo;
+
+/**
+ * Created by caoping on 16/9/29.
+ */
+public class HelloWorld {
+
+    public static void main(String [] args){
+
+        System.out.print("Helloworld");
+    }
+}
